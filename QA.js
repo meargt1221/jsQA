cdddcf83c07756f84ff856f1c26264724813c811
mearg <meargtakelle@gmail.com>
2024-09-08 22:23:48 +0300

    commit5

--- QA.js
+++ QA.js
@@ -145,33 +145,79 @@
 
 
 // Let us take an iteration variable, that represent the remaining Time in seconds
-let n = 60
+// let n = 60
 
-// While loop with a stop condition
-while (n >= 0) {
-    if (n === 50) {
-        console.log("Orbiter transfers from ground to internal power");
-    }
-    else if (n === 31) {
-        console.log("Ground launch sequencer is go for auto sequence start");
-    }
-    else if (n === 16) {
-        console.log("Activate launch pad sound suppression system");
-    }
-    else if (n === 10) {
-        console.log("Activate main engine hydrogen burnoff system");
-    }
-    else if (n === 6) {
-        console.log("Main engine start");
-    }
-    else if (n === 0) {
-        console.log("Solid rocket booster ignition and liftoff!");
-    }
-    else{
-        console.log("T-"+n+" seconds");
-    }
+// // While loop with a stop condition
+// while (n >= 0) {
+//     if (n === 50) {
+//         console.log("Orbiter transfers from ground to internal power");
+//     }
+//     else if (n === 31) {
+//         console.log("Ground launch sequencer is go for auto sequence start");
+//     }
+//     else if (n === 16) {
+//         console.log("Activate launch pad sound suppression system");
+//     }
+//     else if (n === 10) {
+//         console.log("Activate main engine hydrogen burnoff system");
+//     }
+//     else if (n === 6) {
+//         console.log("Main engine start");
+//     }
+//     else if (n === 0) {
+//         console.log("Solid rocket booster ignition and liftoff!");
+//     }
+//     else{
+//         console.log("T-"+n+" seconds");
+//     }
     
-    //Never forget to decrement/increment the iteration variable in a while loop
-    // Otherwise, you loop will run infinite iterations
-    n = n-1;
+//     //Never forget to decrement/increment the iteration variable in a while loop
+//     // Otherwise, you loop will run infinite iterations
+//     n = n-1;
+// }
+
+
+// UdacityFamily
+
+// let udacityFamily = ["Mearg","Julia","James"];
+
+// for (let index=0; index < udacityFamily.length; index++){
+// console.log(udacityFamily[index]);
+// }
+
+
+// let x=1;
+//  for(let i = 1; i <= 12 ; i++) {
+//   x*=i
+// }
+// console.log(x);
+
+// function factorial(n){
+//   if (n===0 || n===1){
+//     return 1;
+//   } else {
+//   return n*factorial(n-1);
+// }
+// }
+// console.log(factorial(12));
+
+
+
+// function x(r){
+//   if (r===0||r===1){
+//     return 1;
+//   }else{
+//     return r*x(r-1);
+//   }
+// }
+// console.log(x(3));
+
+
+function laugh(num){
+  let txt= ""
+  for (let i = 1; i<=num; i++) {
+    txt += "ha"
+    return txt;
+  }
 }
+console.log(laugh()+"!")
