9bf0c8b3828b6b323f42bf0156897ba48288c375
mearg <meargtakelle@gmail.com>
2024-09-13 22:04:18 +0300

    splice

--- QA.js
+++ QA.js
@@ -263,14 +263,19 @@
 
 // Price
 
-const prices = [1.23, 48.11, 90.11, 8.50, 9.99, 1.00, 1.10, 67.00];
-for (let index = 0; index < prices.length; index++) {
-    if (index === 0) {
-    prices[index] = prices[index] * 2;
-} else if (index === 2) {
-    prices[index] = prices[index] * 2;
-} else if (index=== 6) {
-    prices[index] = prices[index] * 2;
-}
-}
-console.log(JSON.stringify(prices));
+// const prices = [1.23, 48.11, 90.11, 8.50, 9.99, 1.00, 1.10, 67.00];
+// for (let index = 0; index < prices.length; index++) {
+//     if (index === 0) {
+//     prices[index] = prices[index] * 2;
+// } else if (index === 2) {
+//     prices[index] = prices[index] * 2;
+// } else if (index=== 6) {
+//     prices[index] = prices[index] * 2;
+// }
+// }
+// console.log(JSON.stringify(prices));
+
+
+const donuts = ["glazed", "chocolate frosted", "Boston creme", "glazed cruller"];
+donuts.splice(1, 1, "chocolate cruller", "creme de leche");
+ 
