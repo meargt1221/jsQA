0770f23e64a681ec0b2f52104233b0b9d17b7ad2
mearg <meargtakelle@gmail.com>
2024-09-14 14:19:06 +0300

    wow

--- QA.js
+++ QA.js
@@ -285,11 +285,19 @@
 // donuts.pop(); // pops "sprinkled" off the end of the `donuts` array
 // donuts.pop(); // pops "cinnamon sugar" off the end of the `donuts` array
 
+// const donuts = ["jelly donut", "chocolate donut", "glazed donut"];
+
+// // the variable `i` is used to step through each element in the array
+// for (let i = 0; i < donuts.length; i++) {
+//     donuts[i] += " hole";
+//     donuts[i] = donuts[i].toUpperCase();
+// }
+// console.log(donuts);
+
 const donuts = ["jelly donut", "chocolate donut", "glazed donut"];
 
-// the variable `i` is used to step through each element in the array
-for (let i = 0; i < donuts.length; i++) {
-    donuts[i] += " hole";
-    donuts[i] = donuts[i].toUpperCase();
-}
-console.log(donuts);
+donuts.forEach(function(donut) {
+  donut += " hole";
+  donut = donut.toUpperCase();
+  console.log(donut);
+});
