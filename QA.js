739a4d951edb3548840fb016d504f6ab80207619
mearg <meargtakelle@gmail.com>
2024-09-14 13:28:42 +0300

    splice

--- QA.js
+++ QA.js
@@ -276,6 +276,19 @@
 // console.log(JSON.stringify(prices));
 
 
-const donuts = ["glazed", "chocolate frosted", "Boston creme", "glazed cruller"];
-donuts.splice(1, 1, "chocolate cruller", "creme de leche");
+// const donuts = ["glazed", "chocolate frosted", "Boston creme", "glazed cruller"];
+// donuts.splice(1, 1, "chocolate cruller", "creme de leche");
  
+// const donuts = ["glazed", "chocolate frosted", "Boston creme", "glazed cruller", "cinnamon sugar", "sprinkled", "powdered"];
+
+// donuts.pop(); // pops "powdered" off the end of the `donuts` array
+// donuts.pop(); // pops "sprinkled" off the end of the `donuts` array
+// donuts.pop(); // pops "cinnamon sugar" off the end of the `donuts` array
+
+function hasEnoughPlayers(arrayInstance) {
+    if (arrayInstance.length >= 7) {
+        return true; // true is a boolean value. Do not return a string "true"
+   } else {
+        return false; // false is also a boolean value. Do not return a string "false"
+   }
+}
