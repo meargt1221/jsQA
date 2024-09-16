5ae7c05c4671804618831dbb7ab79754ca193a8a
mearg <meargtakelle@gmail.com>
2024-09-16 19:05:17 +0300

    interesting

--- QA.js
+++ QA.js
@@ -382,10 +382,38 @@
 
 // console.log(sister.name);
 
+
+
+
+
 // Object literal
-var breakfast = {
-  name: "Lumberjack",
-  price: "$ 9.95",
-  ingredients: ['eggs', 'sausage','toast', 'hashbrowns', 'pancakes']
-}
-console.log(breakfast);
+// var breakfast = {
+//   name: "Lumberjack",
+//   price: "$ 9.95",
+//   ingredients: ['eggs', 'sausage','toast', 'hashbrowns', 'pancakes']
+// }
+// console.log(breakfast);
+
+
+
+
+// Bank accounnts 
+
+var savingsAccount = {
+   balance: 1000,
+   interestRatePercent: 1,
+   deposit: function addMoney(amount) {
+    if (amount >  0) {
+      savingsAccount.balance += amount;
+    }
+   },
+   withdraw: function removeMoney(amount) {
+    var verifyBalance = savingsAccount.balance - amount;
+    if (amount > 0 && verifyBalance >= 0) {
+      savingsAccount.balance -= amount;
+    }
+   },
+  printAccountSummary: function() {
+    return "Welcome!\nYour balance is currently $"+ savingsAccount.balance +" and your interest rate is " + savingsAccount.interestRatePercent + "%."}
+};
+console.log(savingsAccount.printAccountSummary());
